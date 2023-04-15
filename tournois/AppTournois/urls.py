eade64138cfbc6c2cadd5fd7a4e0a7a3e97d94ba
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-15 18:54:09 +0200

    Setting the first view, template, style and base.html to take a first look at /tournament/

--- tournois/AppTournois/urls.py
+++ tournois/AppTournois/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import tournois
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('tournois/', tournois, name='tournois')
+    
 ]
--- tournois/AppTournois/views.py
+++ tournois/AppTournois/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tournoi
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tournament index.")
+
+def tournois(request):
+    tournois = Tournoi.objects.all()
+    context = {
+        'tournois': tournois,
+    }
+    return render(request, 'tournois/tournois.html', context)
