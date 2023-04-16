50e23d922a261aa5f18c63cecc69d1ff77a8add7
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-16 19:36:36 +0200

    adding empty path to default

--- tournois/AppTournois/urls.py
+++ tournois/AppTournois/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import tournois
 
 urlpatterns = [
+    path('', tournois, name='tournois'),
     path('tournoi/<int:tournoi_id>/', views.tournoi, name='tournoi'),
     path('tournois/', tournois, name='tournois'),
     path('poule/<int:poule_id>/', views.poule, name='detail_poule'),    
