4d84ccf088317a1a7964b52fd5a50df025a6cd62
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-17 15:50:13 +0200

    Adding login auth page. Not styled yet

--- tournois/tournois/urls.py
+++ tournois/tournois/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("AppTournois.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
