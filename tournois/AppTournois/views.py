4789cd2f41fb402949d989cc0da2e4c93980bfa1
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-18 16:46:36 +0200

    adding a separate view and templates for comments

--- tournois/AppTournois/views.py
+++ tournois/AppTournois/views.py
@@ -4,6 +4,23 @@ from .models import Tournoi, Poule, commentaire, Match
 from django.contrib.auth.decorators import login_required
 from .forms import CommentaireForm
 
+
+@login_required
+def commenter(request, match_id):
+    match = get_object_or_404(Match, id=match_id)
+    if request.method == 'POST':
+        form = CommentaireForm(request.POST)
+        if form.is_valid():
+            commentaire = form.save(commit=False)
+            commentaire.auteur = request.user
+            commentaire.match = match
+            commentaire.save()
+            return redirect('tournois/match', match_id=match.id)
+    else:
+        form = CommentaireForm()
+    context = {'match': match, 'form': form}
+    return render(request, 'tournois/commenter.html', context)
+
 def tournois(request):
     tournois = Tournoi.objects.all()
     context = {
@@ -31,3 +48,5 @@ def match(request, match_id):
     match = get_object_or_404(Match, id=match_id)
     context = {'match': match}
     return render(request, 'tournois/match.html', context)
+
+
